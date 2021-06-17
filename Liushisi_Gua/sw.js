c8de5decf59b07c40b06b03400048487899ced45
PrunusSerrulata <say635231439@gmail.com>
2021-06-17 19:48:32 +0800

    fix miscode

--- Liushisi_Gua/sw.js
+++ Liushisi_Gua/sw.js
@@ -1,14 +1,14 @@
 var CACHE_VERSION = 'liushisigua_v1';
 var CACHE_FILES = [
     '/Liushisi_Gua/',
-    '/Liushisi_Gua/index.htm',
+    '/Liushisi_Gua/index.html',
     '/Liushisi_Gua/index.js',
     '/Liushisi_Gua/index.css'
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function (e) {
     console.log('[Service Worker] Installing');
-    event.waitUntil(
+    e.waitUntil(
         caches.open(CACHE_VERSION)
             .then(function (cache) {
                 console.log('[Service Worker] Caching all content')
@@ -17,8 +17,8 @@ self.addEventListener('install', function (event) {
     );
 });
 
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
         caches.keys().then(function (keys) {
             return Promise.all(keys.map(function (key) {
                 if (CACHE_FILES.indexOf(key) === -1) {
@@ -29,11 +29,11 @@ self.addEventListener('activate', function (event) {
     );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (r) {
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
             console.log('[Service Worker] Fetching resource: ' + e.request.url);
-            return r || fetch(event.request).then(function (response) {
+            return r || fetch(e.request).then(function (response) {
                 return caches.open(CACHE_FILES).then(function (cache) {
                     console.log('[Service Worker] Caching new resource: ' + e.request.url);
                     cache.put(e.request, response.clone())
