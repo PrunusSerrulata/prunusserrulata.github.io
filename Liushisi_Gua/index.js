0826ac8d35f00e9f6068915ba43405b929519f3a
PrunusSerrulata <say635231439@gmail.com>
2021-06-17 19:11:53 +0800

    update Liushisi_Gua/index.htm

--- Liushisi_Gua/index.js
+++ Liushisi_Gua/index.js
@@ -30,18 +30,6 @@ function guatoB64(str) {
 
 
 window.onload = function () {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function () {
-            navigator.serviceWorker.register('/sw.js', { scope: '/' })
-                .then(function (registration) {
-                    console.log('ServiceWorker registration successful');
-                })
-                .catch(function (err) {
-                    console.log('ServiceWorker registration failed');
-                });
-        });
-    }
-
     plainTextarea = document.getElementById("plain")
     cipherTextarea = document.getElementById("cipher")
 }
