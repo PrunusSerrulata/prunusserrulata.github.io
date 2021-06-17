62d1beb80dbf24ad0d42027aaa8a4ea7d609f422
PrunusSerrulata <say635231439@gmail.com>
2021-06-17 18:48:45 +0800

    update Liushisi_Gua

--- Liushisi_Gua/index.js
+++ Liushisi_Gua/index.js
@@ -1,8 +1,10 @@
 "use strict";
 
 var plainTextarea, cipherTextarea
-var b64Table = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,0,1,2,3,4,5,6,7,8,9,\\+,/".split(",")
-var guaTable = "䷀,䷁,䷂,䷃,䷄,䷅,䷆,䷇,䷈,䷉,䷊,䷋,䷌,䷍,䷎,䷏,䷐,䷑,䷒,䷓,䷔,䷕,䷖,䷗,䷘,䷙,䷚,䷛,䷜,䷝,䷞,䷟,䷠,䷡,䷢,䷣,䷤,䷥,䷦,䷧,䷨,䷩,䷪,䷫,䷬,䷭,䷮,䷯,䷰,䷱,䷲,䷳,䷴,䷵,䷶,䷷,䷸,䷹,䷺,䷻,䷼,䷽,䷾,䷿".split(",")
+const b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split("")
+const gua = "䷀䷁䷂䷃䷄䷅䷆䷇䷈䷉䷊䷋䷌䷍䷎䷏䷐䷑䷒䷓䷔䷕䷖䷗䷘䷙䷚䷛䷜䷝䷞䷟䷠䷡䷢䷣䷤䷥䷦䷧䷨䷩䷪䷫䷬䷭䷮䷯䷰䷱䷲䷳䷴䷵䷶䷷䷸䷹䷺䷻䷼䷽䷾䷿".split("")
+const table = Object.assign.apply({}, Array.prototype.map.call(b64, (v, i) => {return {[v]: gua[i]}}))
+const revtable = Object.assign.apply({}, Array.prototype.map.call(gua, (v, i) => {return {[v]: b64[i]}}))
 
 function b64EncodeUnicode(str) {
     return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
@@ -18,20 +20,11 @@ function b64DecodeUnicode(str) {
 }
 
 function b64toGua(str) {
-    var res = str
-    for (let i = 0; i < b64Table.length; i++) {
-        res = res.replace(new RegExp(b64Table[i], "g"), guaTable[i])
-    }
-    return res.replace(/=/g, "")
+    return Array.prototype.map.call(str, c => table[c]).join("")
 }
 
 function guatoB64(str) {
-    var res = str
-    var remain = str.length % 4
-    for (let i = 0; i < guaTable.length; i++) {
-        res = res.replace(new RegExp(guaTable[i], "g"), b64Table[i])
-    }
-    return res + "=".repeat(remain == 0 ? 0 : 4-remain)
+    return Array.prototype.map.call(str, c => revtable[c]).join("")
 }
 
 
